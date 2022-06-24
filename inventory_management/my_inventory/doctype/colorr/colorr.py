03511d4341338b692fb346220f4a4ecedf576bb5
Robera16 <roberaworknehrobwork@gmail.com>
2022-06-25 00:55:32 +0300

    initial commit

---
+++ inventory_management/my_inventory/doctype/colorr/colorr.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Robera Workneh and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Colorr(Document):
+	pass
---
+++ inventory_management/my_inventory/doctype/stockk/stockk.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Robera Workneh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Stockk', {
+	// refresh: function(frm) {
+
+	// }
+});
